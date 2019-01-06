3507576bb168c911ef7b5dcc62d0b906f38830fd
Erik Osterman <erik@cloudposse.com>
2019-01-05 19:05:27 -0800

    remove invalid params

--- outputs.tf
+++ outputs.tf
@@ -23,11 +23,6 @@ output "security_group_id" {
   description = "ID of the Security Group"
 }
 
-output "parameter_group_id" {
-  value       = "${join("", aws_db_parameter_group.default.*.id)}"
-  description = "ID of the Parameter Group"
-}
-
 output "hostname" {
   value       = "${module.dns_host_name.hostname}"
   description = "DNS host name of the instance"
